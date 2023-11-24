7b519deaaf2632c7b257c68307f1eb5f7dc1301a
dim-anis <anis.dim@gmail.com>
2023-11-24 15:12:08 +0700

    remove unused types

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,23 +1,3 @@
-import { Folder, Recipe } from "@prisma/client";
-
-export type PizzaRecipe = {
-  id: number;
-  name: string;
-  ingredients: {
-    flour: number;
-    water: number;
-    salt?: number;
-    yeast?: number;
-    oil?: number;
-    sugar?: number;
-  };
-  settings: {
-    number_of_pizzas: number;
-    weight_per_pizza: number;
-    hydration: number;
-  };
-};
-
 export type RecipeParsed = {
   id: string;
   name: string;
@@ -31,9 +11,3 @@ export type RecipeParsed = {
     sugar: number;
   };
 };
-
-export type AllRecipesFolder = Omit<
-  RecipeFolder,
-  "userId" | "createdAt" | "updatedAt"
->;
-export type RecipeFolder = Folder & { recipes: Recipe[] };
