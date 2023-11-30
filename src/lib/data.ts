8a0af4c987715228f05f852c53c3f05803de9fd6
dim-anis <anis.dim@gmail.com>
2023-11-30 22:28:53 +0700

    move reused data to a separate file

---
+++ src/lib/data.ts
@@ -0,0 +1,29 @@
+export const ingredients = [
+  {
+    id: "flourAmount",
+    label: "Flour",
+  },
+  {
+    id: "waterAmount",
+    label: "Water",
+  },
+] as const;
+
+export const optionalIngredients = [
+  {
+    id: "saltAmount",
+    label: "Salt",
+  },
+  {
+    id: "yeastAmount",
+    label: "Yeast",
+  },
+  {
+    id: "oilAmount",
+    label: "Oil",
+  },
+  {
+    id: "sugarAmount",
+    label: "Sugar",
+  },
+] as const;
