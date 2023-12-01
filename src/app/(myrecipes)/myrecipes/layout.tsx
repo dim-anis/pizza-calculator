0e2312554fb3e4c8d079ead5f3d6dcbab0f7b20e
dim-anis <anis.dim@gmail.com>
2023-12-01 21:22:58 +0700

    tweak layout

--- src/app/(myrecipes)/myrecipes/layout.tsx
+++ src/app/(myrecipes)/myrecipes/layout.tsx
@@ -12,14 +12,14 @@ export default function MyRecipesLayout({
       <header className="container z-40 bg-background">
         <MainNav />
       </header>
-      <div className="container">
+      <div className="container flex-1">
         <div className="mt-5 space-y-0.5 border-b pb-5">
           <h1 className="text-2xl font-bold tracking-tight">My recipes</h1>
           <p className="text-muted-foreground">
             Create recipes and organize them in folders.
           </p>
         </div>
-        <div className="flex-1 md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10">
+        <div className="md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10">
           <FolderList />
           <div className="relative space-y-2 py-6 md:space-y-4 lg:gap-10 lg:py-10">
             {children}
