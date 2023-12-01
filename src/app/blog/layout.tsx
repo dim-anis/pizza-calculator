284039e744f06ebc0e001432582a38b875b523c6
dim-anis <anis.dim@gmail.com>
2023-12-01 18:50:48 +0700

    remove duplicates after restructuring

--- src/app/blog/layout.tsx
+++
@@ -1,5 +0,0 @@
-type BlogLayoutProps = { children: React.ReactNode };
-
-export default function Layout({ children }: BlogLayoutProps) {
-  return <div className="mx-auto max-w-5xl">{children}</div>;
-}
--- src/app/faq/layout.tsx
+++
@@ -1,5 +0,0 @@
-type FAQLayoutProps = { children: React.ReactNode };
-
-export default function Layout({ children }: FAQLayoutProps) {
-  return <div className="mx-auto max-w-5xl">{children}</div>;
-}
