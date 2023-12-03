7e6863110f978437fcad6639f33ac7110fddc1f0
dim-anis <anis.dim@gmail.com>
2023-12-03 14:18:28 +0700

    truncate folder names in folder list

--- src/app/(myrecipes)/myrecipes/[folder]/_components/folder-list-item.tsx
+++ src/app/(myrecipes)/myrecipes/[folder]/_components/folder-list-item.tsx
@@ -20,9 +20,9 @@ export default function FolderItem({
           isSelected
             ? "bg-muted hover:bg-muted"
             : "bg-transparent hover:bg-muted"
-        } inline-flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md bg-muted px-4 py-2 text-sm font-medium transition-colors hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50`}
+        } inline-flex h-9 w-full items-center justify-between rounded-md bg-muted px-4 py-2 text-sm font-medium transition-colors hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50`}
       >
-        {folderName}
+        <span className="truncate">{folderName}</span>
         <span className="flex w-6 justify-center rounded-full bg-slate-200 px-2 py-1 text-xs">
           {recipeCount}
         </span>
