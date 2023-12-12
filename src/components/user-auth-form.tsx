d093c42d76403a9893e769ffbb6a452a109b4cf4
dim-anis <anis.dim@gmail.com>
2023-12-12 20:13:21 +0700

    fix typo

--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -41,7 +41,7 @@ export default function UserAuthForm() {
           }}
           disabled={isLoading || isGithubLoading || isGoogleLoading}
         >
-          {isGithubLoading ? (
+          {isGoogleLoading ? (
             <Loader2 className="mr-2 h-4 w-4 animate-spin" />
           ) : (
             <svg
