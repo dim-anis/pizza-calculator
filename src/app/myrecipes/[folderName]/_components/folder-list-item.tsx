c1bcf164d2aee06a847aad26dba5dec3e4bd5f8e
dim-anis <anis.dim@gmail.com>
2025-03-28 12:25:48 +0700

    refactor(FolderList): make selected item text bolder

--- src/app/myrecipes/[folderName]/_components/folder-list-item.tsx
+++ src/app/myrecipes/[folderName]/_components/folder-list-item.tsx
@@ -15,9 +15,9 @@ export default function FolderItem({ folderName, recipeCount, href }: Props) {
       <li
         className={`${
           path === href
-            ? "bg-muted hover:bg-muted"
+            ? "bg-muted hover:bg-muted font-medium"
             : "bg-transparent hover:bg-muted"
-        } inline-flex h-9 w-full items-center justify-between rounded-md bg-muted px-4 py-2 text-sm font-medium transition-colors hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50`}
+        } inline-flex h-9 w-full items-center justify-between rounded-md bg-muted px-4 py-2 text-sm transition-colors hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50`}
       >
         <span className="truncate">{folderName}</span>
         <span className="flex w-6 justify-center rounded-full bg-slate-200 px-2 py-1 text-xs">
