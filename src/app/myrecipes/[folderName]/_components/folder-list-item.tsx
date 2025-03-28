db1709273806d297ce2d42186d3a72ed21905371
dim-anis <anis.dim@gmail.com>
2025-03-28 12:26:26 +0700

    refactor(FolderList): add left margin to folder count

--- src/app/myrecipes/[folderName]/_components/folder-list-item.tsx
+++ src/app/myrecipes/[folderName]/_components/folder-list-item.tsx
@@ -20,7 +20,7 @@ export default function FolderItem({ folderName, recipeCount, href }: Props) {
         } inline-flex h-9 w-full items-center justify-between rounded-md bg-muted px-4 py-2 text-sm transition-colors hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50`}
       >
         <span className="truncate">{folderName}</span>
-        <span className="flex w-6 justify-center rounded-full bg-slate-200 px-2 py-1 text-xs">
+        <span className="ml-2 flex w-6 justify-center rounded-full bg-slate-200 px-2 py-1 text-xs">
           {recipeCount}
         </span>
       </li>
