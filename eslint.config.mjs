519130008b8a6ab665e6cf8a25ab0597c3419136
dim-anis <anis.dim@gmail.com>
2025-04-11 10:53:26 +0700

    use import.meta.dirname

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,6 @@
-// import tailwind from "eslint-plugin-tailwindcss";
-//
-// import { FlatCompat } from "@eslint/eslintrc";
-//
-// const compat = new FlatCompat({
-//   baseDirectory: import.meta.dirname,
-// });
-//
-// const eslintConfig = [
-//   ...compat.config({
-//     extends: [
-//       "next/core-web-vitals",
-//       ...tailwind.configs["flat/recommended"],
-//       "next/typescript",
-//     ],
-//   }),
-// ];
-//
-// export default eslintConfig;
-
 import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = import.meta.dirname;
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
