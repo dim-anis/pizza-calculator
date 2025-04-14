bc4cde1a9175dab2e692f14a884026d1202aa1c4
dim-anis <anis.dim@gmail.com>
2025-04-14 12:38:02 +0700

    refactor: remove unused link styles

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -20,17 +20,12 @@ export default async function Home() {
             <Link
               className={`${buttonVariants({
                 variant: "secondary",
-              })} inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50`}
+              })}`}
               href={`/dashboard/recipes/new`}
             >
               Create recipe
             </Link>
-            <Link
-              className={`${buttonVariants({
-                variant: "default",
-              })} inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50`}
-              href="#calculator"
-            >
+            <Link className={`${buttonVariants()}`} href="#calculator">
               Calculate
             </Link>
           </div>
