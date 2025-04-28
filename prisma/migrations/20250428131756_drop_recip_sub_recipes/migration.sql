b2a7f547283b033c02bbe755ac8188b8eca536e9
dim-anis <anis.dim@gmail.com>
2025-04-28 20:17:17 +0700

    refactor(schema): drop recipeSubRecipes

---
+++ prisma/migrations/20250428131756_drop_recip_sub_recipes/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the `recipe_sub_recipes` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the column `isPreferment` on the `recipes` table. All the data in the column will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "recipe_sub_recipes";
+PRAGMA foreign_keys=on;
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_recipes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT,
+    "name" TEXT NOT NULL,
+    "notes" TEXT,
+    "servings" INTEGER NOT NULL DEFAULT 1,
+    CONSTRAINT "recipes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_recipes" ("createdAt", "id", "name", "notes", "servings", "updatedAt", "userId") SELECT "createdAt", "id", "name", "notes", "servings", "updatedAt", "userId" FROM "recipes";
+DROP TABLE "recipes";
+ALTER TABLE "new_recipes" RENAME TO "recipes";
+CREATE INDEX "recipes_userId_idx" ON "recipes"("userId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
