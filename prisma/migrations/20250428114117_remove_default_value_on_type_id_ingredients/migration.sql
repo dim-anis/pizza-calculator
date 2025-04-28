c67f367a9d7f2845759b95d5379a7cc3983b006f
dim-anis <anis.dim@gmail.com>
2025-04-28 18:39:28 +0700

    remove default value for typeId col on Ingredient

---
+++ prisma/migrations/20250428114117_remove_default_value_on_type_id_ingredients/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_ingredients" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "typeId" INTEGER NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "ingredients_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "ingredients_typeId_fkey" FOREIGN KEY ("typeId") REFERENCES "ingredient_types" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_ingredients" ("id", "name", "typeId", "userId") SELECT "id", "name", "typeId", "userId" FROM "ingredients";
+DROP TABLE "ingredients";
+ALTER TABLE "new_ingredients" RENAME TO "ingredients";
+CREATE UNIQUE INDEX "ingredients_userId_name_key" ON "ingredients"("userId", "name");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
